d3e923906ae0abed42fd55b7231b518fce62e999
Abdiel Martinez <aabdiel1998@gmail.com>
2023-10-12 15:38:46 -0600

    ✅ test(app.controller.spec.ts): add tests for getJson method

--- challenge/src/app.controller.spec.ts
+++ challenge/src/app.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotFoundException, BadRequestException } from '@nestjs/common';
+import mockedShortJSON from '../__mocks__/shortJson';
+import mockedLongJSON from '../__mocks__/longJson';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,8 +18,55 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should throw BadRequestException when url param is missing', async () => {
+      expect.assertions(2);
+      try {
+        await appController.getJson();
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.message).toBe('url param is missing');
+      }
+    });
+
+    it('should throw NotFoundException when file not found', async () => {
+      expect.assertions(1);
+      try {
+        await appController.getJson('example');
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
+
+    it('should return json when found email file with json attached', async () => {
+      expect(
+        await appController.getJson(
+          '../challenge/example-emails/JSON file attached.eml',
+        ),
+      ).toEqual(mockedShortJSON);
+    });
+
+    it('should return json when found email file with json in body link', async () => {
+      expect(
+        await appController.getJson(
+          '../challenge/example-emails/JSON in the body as a link.eml',
+        ),
+      ).toEqual(mockedShortJSON);
+    });
+
+    it('should return json when found email file with a link that leads to a web where there is a link that leads to the actual JSON', async () => {
+      expect(
+        await appController.getJson(
+          '../challenge/example-emails/JSON in a link that leads to a web where there is a link that leads to the actual JSON.eml',
+        ),
+      ).toEqual(mockedLongJSON);
+    }, 12000);
+
+    it('should return json when found a link to an email file with json attached', async () => {
+      expect(
+        await appController.getJson(
+          'https://raw.githubusercontent.com/Anb98/NestJS-Test/master/challenge/example-emails/JSON%20file%20attached.eml',
+        ),
+      ).toEqual(mockedShortJSON);
     });
   });
 });
